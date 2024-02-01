7cd09dbdcaafd2fa0ccce07abe58fd6921818118
Murilo Elias <muriloelias@ipeweb.com.br>
2024-02-01 16:33:38 -0300

    Fixes

--- src/Pages/Cards/Card.jsx
+++ src/Pages/Cards/Card.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Cards.css";
+import { Link } from "react-router-dom";
 
 export default function Card({ cardTitle, cardId, onClick, isCreate }) {
 
     return (
-        <div className="card-container" onClick={onClick}>
-            <div className="card-paper"><div className="card-paper-text" style={isCreate && { color: "#989898" }}>{cardTitle}</div></div>
-            <div className="card-paper-shadow"></div>
-        </div>
+        <Link>
+            <div className="card-container" onClick={onClick}>
+                <div className="card-paper"><div className="card-paper-text" style={isCreate && { color: "#989898" }}>{cardTitle}</div></div>
+                <div className="card-paper-shadow"></div>
+            </div>
+        </Link>
     );
 }
