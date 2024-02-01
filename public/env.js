0a7a8688628982a89bedd82b89b3001789dc0465
iloElias <murilo7456@gmail.com>
2024-02-01 19:40:29 -0300

    Fixes

--- public/env.js
+++
@@ -1,5 +0,0 @@
-window.env = {
-  "API_URL": "https://recap-backend-production.up.railway.app/public",
-  "GOOGLE_API_KEY": "640581536870-a1dsnhg4iv2llnv6sfj4iboqc0aoh0ei.apps.googleusercontent.com",
-  "LOCALHOST": "true"
-};
