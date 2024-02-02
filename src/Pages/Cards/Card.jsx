9bf8a289ad522cb1a2c618e2c03fd3b534d01f43
Murilo Elias <muriloelias@ipeweb.com.br>
2024-02-02 11:58:20 -0300

    Fixes

--- src/Pages/Cards/Card.jsx
+++ src/Pages/Cards/Card.jsx
@@ -7,7 +7,7 @@ export default function Card({ cardTitle, cardId, onClick, isCreate, isLink }) {
     return (
         <>
             {isLink ? (
-                <Link to={"/card/" + cardId} >
+                <Link to={cardId ? ("/card/" + cardId) : "/"} >
                     <div className="card-container" onClick={onClick}>
                         <div className="card-paper-shadow"></div>
                         <div className="card-paper"><div className="card-paper-text" style={isCreate && { color: "#989898" }}>{cardTitle}</div></div>
